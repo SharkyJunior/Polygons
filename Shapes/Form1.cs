2a258a1710fdfd2d586d1c470b040506d3326169
SharkyJunior <67413544+SharkyJunior@users.noreply.github.com>
2022-09-23 20:53:58 +0300

    initial repo creation

---
+++ Shapes/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shapes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Brush b = new SolidBrush(Color.Red);
+            Pen p = new Pen(b);
+            g.FillEllipse(b, 100, 100, 200, 200);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Shapes/Shape.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Shapes
+{
+    internal abstract class Shape
+    {
+        protected int x, y;
+        static int radius;
+        static Color color;
+
+        public abstract void Draw(Graphics g);
+
+        public abstract bool IsInside(int mouseX, int mouseY);
+    }
+
+    internal class Circle : Shape
+    {
+        public Circle(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        static Circle()
+        {
+
+        }
+        public override void Draw(Graphics g)
+        {
+            
+        }
+    }
+}
